b91f7a8c96574d06bdd904c1a741a9082fe2569b
Gabriela Paulino <gabrielagomespaulino@gmail.com>
2022-05-22 18:16:23 +0100

    make script recursive

--- script.sh
+++ script.sh
@@ -1,9 +1,17 @@
-echo "filename,filesize" > output.csv
+echo 'filename,filesize' > output.csv # add csv headers
 
-fileNames=`ls files/`
-for eachfile in $fileNames
-do
-    filesize=$(du -sb "files/$eachfile" | awk '{ print $1 }')
-    echo $eachfile,$filesize >> output.csv
-done
+searchDir() {
+    filenames=`ls $1`
+    for eachfile in $filenames
+    do
+        if [ -d "$1\/$eachfile" ]; then # is directory? iterate recursively
+            searchDir $1\/$eachfile
+        else
+            filesize=$(du -sb "$1\/$eachfile" | awk '{ print $1 }')
+            echo $eachfile,$filesize >> output.csv
+        fi
+    done
+}
 
+firstDir="files"
+searchDir $firstDir
