bbfa4e8dcd0064bc7923a8d21b0de9076fedc571
Gabriela Paulino <gabrielagomespaulino@gmail.com>
2022-05-22 19:48:17 +0100

    implement histogram as a dictionary

--- calculations.py
+++ calculations.py
@@ -1,21 +1,39 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
 def run_calculate():
+    # Create dataframe
     data= pd.read_csv("output.csv")
+
+    # Average file size
     average_file_size= data['filesize'].mean()
+
+    # Biggest file
     biggest_file_index= data['filesize'].idxmax()
-    smallest_file_index= data['filesize'].idxmin()
+    biggest_file = data['filename'][biggest_file_index]
     
-    print("The average file size is: ",average_file_size)
-    print("The biggest file is: ",data['filename'][biggest_file_index])
-    print("The smallest file is: ",data['filename'][smallest_file_index])
-    #data.hist(column='filesize')
-    #plt.show()
- 
+    # Smallest file
+    smallest_file_index = data['filesize'].idxmin()
+    smallest_file = data['filename'][smallest_file_index]
+    
+    # Output to console
+    print("The average file size is: ", average_file_size)
+    print("The biggest file is: ", biggest_file)
+    print("The smallest file is: ", smallest_file)
+    
+    data.hist(column='filesize')
+    #plt.show() # Visual representation of the histogram
+
+    # Values from the histogram
+    count,division = np.histogram(data['filesize'])
+
     return {
         "average": average_file_size,
-        "biggest_file": data['filename'][biggest_file_index],
-        "smallest_file": data['filename'][smallest_file_index]
-       # "histogram":  data.apply(histfun).to_dict()
+        "biggest_file": biggest_file,
+        "smallest_file": smallest_file,
+        "histogram":  {
+            "count": count.tolist(),
+            "filesize": division.tolist()
+        }
     }
