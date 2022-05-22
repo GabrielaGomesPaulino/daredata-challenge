cc806b6f997281c86166b49277233b351fe04f58
Gabriela Paulino <gabrielagomespaulino@gmail.com>
2022-05-22 16:40:59 +0100

    Add function to Calculations

---
+++ Calculations.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+class Calculations():
+    data= pd.read_csv("output.csv")
+    average_file_size= data['filesize'].mean()
+    biggest_file_index= data['filesize'].idxmax()
+    smallest_file_index= data['filesize'].idxmin()
+    print("The average file size is: ",average_file_size)
+    print("The biggest file is: ",data['filename'][biggest_file_index])
+    print("The smallest file is: ",data['filename'][smallest_file_index])
+    data.hist(column='filesize')
+    plt.show()
